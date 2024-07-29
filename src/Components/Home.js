3cab23696659424185df91c7d19a0f2df972b52e
Venkata Sai Kuniganti <venkatasai7677@gmail.com>
2024-07-29 13:05:29 +0530

    first commit

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -4,7 +4,7 @@ import Description from './Description'
 import Timeline from './Timeline'
 import Projects from './Projects'
 import WordCloud from './WordCloud'
-import Experience from './Experience'
+// import Experience from './Experience'
 import Footer from './Footer'
 const Home = () => {
   return (
